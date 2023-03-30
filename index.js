dca40d6a7335e32e62e31699bdcbf9ae595ec3a6
BennyHung <bennyhudsonhung@gmail.com>
2023-03-30 06:53:34 -0700

    completed lab

---
+++ index.js
@@ -0,0 +1,33 @@
+function shout(arg) {
+    return arg.toUpperCase()
+}
+
+function whisper(string) {
+    return string.toLowerCase()
+}
+
+function logShout(string) {
+    let answer = string.toUpperCase()
+    console.log(answer)
+    return answer
+}
+
+function logWhisper(string) {
+    let answer = string.toLowerCase()
+    console.log(answer)
+    return answer
+}
+
+f
+
+function sayHiToHeadphonedRoommate(string) {
+    if (string === string.toLowerCase()) {
+        return `I can't hear you!`
+    } else if (string === string.toUpperCase()) {
+        return `YES INDEED!`
+    } else if (string === `Let's have dinner together!`) {
+        return `I would love to!`
+    }
+}
+        
+        
